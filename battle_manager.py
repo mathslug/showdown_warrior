f7c8cf4dc37577669431d2104bedf4ad7ea06672
betterentley <john.bentley@betterment.com>
2020-07-31 01:37:17 -0400

    surface more info to thinker

--- battle_manager.py
+++ battle_manager.py
@@ -21,28 +21,70 @@ class Gen1Knight():
         if inp_type == 'request' and params != [''] and not '{"wait":' in params[0]:
             params_dict = json.loads(params[0])
             self.__update_team(params_dict)
-            return self.__next_move('forceSwitch' in params_dict.keys())
-        elif inp_type == 'switch' and 'p2' in params[0]:
-            self.__update_opp_mons(params)
+            is_forced_switch, is_forced_stay = self.__is_forced_stay_or_switch(params_dict)
+            return self.__next_move(is_forced_switch, is_forced_stay)
+        elif inp_type in ['switch', 'damage'] and 'p2' in params[0]:
+            self.__switch_update_opp_mons(params[0][5:], params[-1])
+            return asyncio.sleep(0)
+        elif inp_type == 'move' and 'p2' in params[0]:
+            self.__move_update_opp_mons(params[1])
             return asyncio.sleep(0)
         elif inp_type == 'win':
             return self.__end_words(params)
         else:
             return asyncio.sleep(0)
 
+    def __is_forced_stay_or_switch(self, params_dict):
+        if 'forceSwitch' in params_dict.keys():
+            is_forced_switch = True
+            is_forced_stay = False
+        elif len(params_dict['active']) > 1 and params_dict['active'][1] == '"trapped":true':
+            is_forced_switch = False
+            is_forced_stay = True
+        else:
+            is_forced_switch = False
+            is_forced_stay = False
+        return is_forced_switch, is_forced_stay
+
     def __update_team(self, team_dict):
         if 'active' in team_dict.keys():
             self.__big_brain.active_moves_list = team_dict['active'][0]['moves']
         self.__big_brain.pokemon_list = team_dict['side']['pokemon']
 
-    def __update_opp_mons(self, opp_params):
-        opp_mon = opp_params[1]
-        self.__big_brain.opp_active_mon = opp_mon
-        if not opp_mon in self.__big_brain.opp_pokemon_list:
-            self.__big_brain.opp_pokemon_list.append(opp_mon)
+    def __switch_update_opp_mons(self, opp_mon_id, opp_mon_health_str):
+        if opp_mon_id in self.__big_brain.opp_pokemon_dict.keys():
+            opp_single_pokemon_dict = self.__big_brain.opp_pokemon_dict[opp_mon_id]
+        else:
+            opp_single_pokemon_dict = dict()
 
-    def __next_move(self, is_forced_switch):
-        do_switch, my_selection = self.__big_brain.get_next_move(is_forced_switch)
+        if 'fnt' in opp_mon_health_str:
+            opp_single_pokemon_dict['health'] = 0
+            opp_single_pokemon_dict['status'] = 'fnt'
+        else:
+            opp_mon_health_list = opp_mon_health_str.split('/')
+            opp_single_pokemon_dict['health'] = int(opp_mon_health_list[0])
+            opp_mon_health_list_partial = opp_mon_health_list[1].split(' ')
+            opp_single_pokemon_dict['max_health'] = int(opp_mon_health_list_partial[0])
+            if len(opp_mon_health_list_partial) > 1:
+                opp_single_pokemon_dict['status'] = opp_mon_health_list_partial[1]
+            else:
+                opp_single_pokemon_dict['status'] = 'ok'
+        self.__big_brain.opp_active_mon = opp_mon_id
+        self.__big_brain.opp_pokemon_dict[opp_mon_id] = opp_single_pokemon_dict
+
+    def __move_update_opp_mons(self, opp_move):
+        opp_single_pokemon_dict = self.__big_brain.opp_pokemon_dict[self.__big_brain.opp_active_mon]
+        if 'moves' in opp_single_pokemon_dict.keys():
+            move_list = opp_single_pokemon_dict['moves']
+            if not opp_move in move_list:
+                move_list.append(opp_move)
+                opp_single_pokemon_dict['moves'] = move_list
+        else:
+            opp_single_pokemon_dict['moves'] = [opp_move]
+        self.__big_brain.opp_pokemon_dict[self.__big_brain.opp_active_mon] = opp_single_pokemon_dict
+
+    def __next_move(self, is_forced_switch, is_forced_stay):
+        do_switch, my_selection = self.__big_brain.get_next_move(is_forced_switch, is_forced_stay)
         if do_switch:
             return self.room_obj.switch(my_selection)
         else:
--- thinker.py
+++ thinker.py
@@ -11,38 +11,24 @@ class Gen1Thinker():
 	def __init__(self):
 		self.active_moves_list = []
 		self.pokemon_list = []
-		self.opp_active_mon = ''
-		self.opp_pokemon_list = []
+		self.opp_active_mon = []
+		self.opp_pokemon_dict = dict()
 
-	def get_next_move(self, is_forced_switch):
-		usable_mons = self.__get_usable_mons()
-		usable_moves = self.__get_usable_moves()
-		will_switch = self.__get_will_switch(is_forced_switch, usable_mons)
-		if will_switch:
-			return will_switch, self.__get_next_mon(usable_mons)
-		else:
-			return will_switch, self.__get_next_attack(usable_moves)
+	def get_next_move(self, is_forced_switch, is_forced_stay):
+		actions = self.__get_possible_actions(is_forced_switch, is_forced_stay)
+		selection = self.__choose_next_action(actions)
+		return selection
 
-	def __get_usable_mons(self):
-		mons_switchable_list = map(lambda mon: not 'fnt' in mon['condition'] and not mon['active'], self.pokemon_list)
-		usable_mons = list(compress(map(lambda mon: mon['ident'][4:], self.pokemon_list), mons_switchable_list))
-		return usable_mons
+	def __get_possible_actions(self, is_forced_switch, is_forced_stay):
+		moves_selectable_list = map(lambda move: not 'pp' in move.keys() or move['pp'] !=  0 and not is_forced_switch, self.active_moves_list)
+		usable_moves = compress(map(lambda move: move['id'], self.active_moves_list), moves_selectable_list)
+		usable_moves = map(lambda move: [False, move], usable_moves)
 
-	def __get_usable_moves(self):
-		moves_selectable_list = map(lambda move: not 'pp' in move.keys() or move['pp'] !=  0, self.active_moves_list)
-		usable_moves = list(compress(map(lambda move: move['id'], self.active_moves_list), moves_selectable_list))
-		return usable_moves
+		mons_switchable_list = map(lambda mon: not 'fnt' in mon['condition'] and not mon['active'] and not is_forced_stay, self.pokemon_list)
+		usable_mons = compress(map(lambda mon: mon['ident'][4:], self.pokemon_list), mons_switchable_list)
+		usable_mons =  map(lambda mon: [True, mon], usable_mons)
 
-	def __get_will_switch(self, is_forced_switch, usable_mons):
-		if is_forced_switch:
-			return True
-		elif not usable_mons:
-			return False
-		else:
-			return random.choice([True, False, False, False, False, False, False])
+		return list(usable_mons) + list(usable_moves)
 
-	def __get_next_mon(self, usable_mons):
-		return str(random.choice(usable_mons))
-
-	def __get_next_attack(self, usable_moves):
-		return str(random.choice(usable_moves))
+	def __choose_next_action(self, actions):
+		return random.choice(actions)
