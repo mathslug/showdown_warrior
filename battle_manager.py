b39f5b132582c224c5f962c2ab4d52f9778a4774
betterentley <john.bentley@betterment.com>
2020-08-01 02:04:43 -0400

    two predictors

--- battle_manager.py
+++ battle_manager.py
@@ -12,6 +12,9 @@ class Gen1Knight():
         self.room_obj = room_obj
         self.__big_brain = Gen1Thinker()
         self.__username = username
+        self.__turn_counter = 0
+        self.__is_forced_switch = False
+        self.__is_forced_stay = False
 
     def process_incoming(self, inp_type, params):
         print(inp_type)
@@ -21,16 +24,24 @@ class Gen1Knight():
         if inp_type == 'request' and params != [''] and not '{"wait":' in params[0]:
             params_dict = json.loads(params[0])
             self.__update_team(params_dict)
-            is_forced_switch, is_forced_stay = self.__is_forced_stay_or_switch(params_dict)
-            return self.__next_move(is_forced_switch, is_forced_stay)
-        elif inp_type in ['switch', 'damage'] and 'p2' in params[0]:
-            self.__switch_update_opp_mons(params[0][5:], params[-1])
+            self.__is_forced_switch, self.__is_forced_stay = self.__is_forced_stay_or_switch(params_dict)
+            return asyncio.sleep(0)
+        elif inp_type in ['switch', '-damage']:
+            self.__switch_damage_update_mons(params[0][5:], params[-1], 'p2' in params[0])
             return asyncio.sleep(0)
         elif inp_type == 'move' and 'p2' in params[0]:
             self.__move_update_opp_mons(params[1])
             return asyncio.sleep(0)
         elif inp_type == 'win':
             return self.__end_words(params)
+        elif inp_type == 'turn':
+            self.__turn_counter = int(params[0])
+            return self.__next_move()
+        elif inp_type == 'faint' and 'p1' in params[0]:
+            return self.__next_move()
+        elif inp_type == '-status' and 'p2' in params[0]:
+            self.__big_brain.opp_pokemon_dict[self.__big_brain.opp_active_mon]['status'] = params[1]
+            return asyncio.sleep(0)
         else:
             return asyncio.sleep(0)
 
@@ -38,7 +49,7 @@ class Gen1Knight():
         if 'forceSwitch' in params_dict.keys():
             is_forced_switch = True
             is_forced_stay = False
-        elif len(params_dict['active']) > 1 and params_dict['active'][1] == '"trapped":true':
+        elif 'trapped' in params_dict['active'][0].keys() and params_dict['active'][0]['trapped']:
             is_forced_switch = False
             is_forced_stay = True
         else:
@@ -49,42 +60,75 @@ class Gen1Knight():
     def __update_team(self, team_dict):
         if 'active' in team_dict.keys():
             self.__big_brain.active_moves_list = team_dict['active'][0]['moves']
-        self.__big_brain.pokemon_list = team_dict['side']['pokemon']
-
-    def __switch_update_opp_mons(self, opp_mon_id, opp_mon_health_str):
-        if opp_mon_id in self.__big_brain.opp_pokemon_dict.keys():
-            opp_single_pokemon_dict = self.__big_brain.opp_pokemon_dict[opp_mon_id]
-        else:
-            opp_single_pokemon_dict = dict()
-
-        if 'fnt' in opp_mon_health_str:
-            opp_single_pokemon_dict['health'] = 0
-            opp_single_pokemon_dict['status'] = 'fnt'
-        else:
-            opp_mon_health_list = opp_mon_health_str.split('/')
-            opp_single_pokemon_dict['health'] = int(opp_mon_health_list[0])
-            opp_mon_health_list_partial = opp_mon_health_list[1].split(' ')
-            opp_single_pokemon_dict['max_health'] = int(opp_mon_health_list_partial[0])
-            if len(opp_mon_health_list_partial) > 1:
-                opp_single_pokemon_dict['status'] = opp_mon_health_list_partial[1]
+        pokemon_list = team_dict['side']['pokemon']
+        pokemon_dict = dict()
+        active_mon_id = ''
+        for mon in pokemon_list:
+            mon_id = mon['ident'][4:]
+            single_pokemon_dict = dict()
+            single_pokemon_dict['active'] = mon['active']
+            if single_pokemon_dict['active']:
+                active_mon_id = mon_id
+            mon_health_str = mon['condition']
+            if 'fnt' in mon_health_str:
+                single_pokemon_dict['health'] = 0
+                single_pokemon_dict['max_health'] = self.__big_brain.pokemon_dict[mon_id]['max_health']
+                single_pokemon_dict['status'] = 'fnt'
             else:
-                opp_single_pokemon_dict['status'] = 'ok'
-        self.__big_brain.opp_active_mon = opp_mon_id
-        self.__big_brain.opp_pokemon_dict[opp_mon_id] = opp_single_pokemon_dict
+                mon_health_list = mon_health_str.split('/')
+                single_pokemon_dict['health'] = int(mon_health_list[0])
+                mon_health_list_partial = mon_health_list[1].split(' ')
+                single_pokemon_dict['max_health'] = int(mon_health_list_partial[0])
+                if len(mon_health_list_partial) > 1:
+                    single_pokemon_dict['status'] = mon_health_list_partial[1]
+                else:
+                    single_pokemon_dict['status'] = 'ok'
+            single_pokemon_dict['moves'] = mon['moves']
+            single_pokemon_dict['stats'] = mon['stats']
+            pokemon_dict[mon_id] = single_pokemon_dict
+        self.__big_brain.active_mon = active_mon_id
+        self.__big_brain.pokemon_dict = pokemon_dict
+
+
+    def __switch_damage_update_mons(self, mon_id, mon_health_str, is_opp_mon):
+        if is_opp_mon and mon_id in self.__big_brain.opp_pokemon_dict.keys():
+            single_pokemon_dict = self.__big_brain.opp_pokemon_dict[mon_id]
+        elif not is_opp_mon and mon_id in self.__big_brain.pokemon_dict.keys():
+            single_pokemon_dict = self.__big_brain.pokemon_dict[mon_id]
+        else:
+            single_pokemon_dict = dict()
+            single_pokemon_dict['moves'] = []
+
+        if 'fnt' in mon_health_str:
+            single_pokemon_dict['health'] = 0
+            single_pokemon_dict['status'] = 'fnt'
+        else:
+            mon_health_list = mon_health_str.split('/')
+            single_pokemon_dict['health'] = int(mon_health_list[0])
+            mon_health_list_partial = mon_health_list[1].split(' ')
+            single_pokemon_dict['max_health'] = int(mon_health_list_partial[0])
+            if len(mon_health_list_partial) > 1:
+                single_pokemon_dict['status'] = mon_health_list_partial[1]
+            else:
+                single_pokemon_dict['status'] = 'ok'
+        if is_opp_mon:
+            self.__big_brain.opp_active_mon = mon_id
+            self.__big_brain.opp_pokemon_dict[mon_id] = single_pokemon_dict
+        else:
+            self.__big_brain.active_mon = mon_id
+            self.__big_brain.pokemon_dict[mon_id] = single_pokemon_dict
 
     def __move_update_opp_mons(self, opp_move):
         opp_single_pokemon_dict = self.__big_brain.opp_pokemon_dict[self.__big_brain.opp_active_mon]
-        if 'moves' in opp_single_pokemon_dict.keys():
-            move_list = opp_single_pokemon_dict['moves']
-            if not opp_move in move_list:
-                move_list.append(opp_move)
-                opp_single_pokemon_dict['moves'] = move_list
-        else:
-            opp_single_pokemon_dict['moves'] = [opp_move]
+        move_list = opp_single_pokemon_dict['moves']
+        if not opp_move in move_list:
+            move_list.append(opp_move)
+            opp_single_pokemon_dict['moves'] = move_list
         self.__big_brain.opp_pokemon_dict[self.__big_brain.opp_active_mon] = opp_single_pokemon_dict
 
-    def __next_move(self, is_forced_switch, is_forced_stay):
-        do_switch, my_selection = self.__big_brain.get_next_move(is_forced_switch, is_forced_stay)
+    def __next_move(self):
+        asyncio.sleep(1)
+        do_switch, my_selection = self.__big_brain.get_next_move(self.__is_forced_switch, self.__is_forced_stay)
         if do_switch:
             return self.room_obj.switch(my_selection)
         else:
--- thinker.py
+++ thinker.py
@@ -9,9 +9,10 @@ from itertools import compress
 
 class Gen1Thinker():
 	def __init__(self):
+		self.active_mon = ''
 		self.active_moves_list = []
-		self.pokemon_list = []
-		self.opp_active_mon = []
+		self.pokemon_dict = dict()
+		self.opp_active_mon = ''
 		self.opp_pokemon_dict = dict()
 
 	def get_next_move(self, is_forced_switch, is_forced_stay):
@@ -24,11 +25,31 @@ class Gen1Thinker():
 		usable_moves = compress(map(lambda move: move['id'], self.active_moves_list), moves_selectable_list)
 		usable_moves = map(lambda move: [False, move], usable_moves)
 
-		mons_switchable_list = map(lambda mon: not 'fnt' in mon['condition'] and not mon['active'] and not is_forced_stay, self.pokemon_list)
-		usable_mons = compress(map(lambda mon: mon['ident'][4:], self.pokemon_list), mons_switchable_list)
+		mons_switchable_list = map(lambda key: 'fnt' != self.pokemon_dict[key]['status'] and not self.pokemon_dict[key]['active'] and not is_forced_stay, self.pokemon_dict.keys())
+		usable_mons = compress(self.pokemon_dict.keys(), mons_switchable_list)
 		usable_mons =  map(lambda mon: [True, mon], usable_mons)
 
 		return list(usable_mons) + list(usable_moves)
 
 	def __choose_next_action(self, actions):
-		return random.choice(actions)
+		action_metrics_list = list(map(self.__get_action_metrics, actions))
+		max_npw_score = max(map(lambda d: d['npw_score'], action_metrics_list))
+		is_best_action_list = list(map(lambda d: d['npw_score'] == max_npw_score, action_metrics_list))
+		best_action_list = list(compress(actions, is_best_action_list))
+		return random.choice(best_action_list)
+
+	def __get_action_metrics(self, action):
+		metrics_dict = dict()
+		metrics_dict['action'] = action
+		print('USE')
+		print(self.opp_pokemon_dict)
+		metrics_dict['self_hp'] = self.pokemon_dict[self.active_mon]['health'] / self.pokemon_dict[self.active_mon]['max_health']
+		metrics_dict['opp_hp'] = self.opp_pokemon_dict[self.opp_active_mon]['health'] / self.opp_pokemon_dict[self.opp_active_mon]['max_health']
+		# TK: add metrics lol
+		metrics_dict['npw_score'] = self.__get_predicted_npw_score(metrics_dict)
+		return metrics_dict
+
+	def __get_predicted_npw_score(self, metrics_dict):
+		# npw = sqrt(1 / # turns to win)
+		# 0 = did not win
+		return 0
