7c4940bdcc07a8913455e211b8cc0ef9b6b3e0d9
betterentley <john.bentley@betterment.com>
2020-09-12 16:57:25 -0400

    more versaatility

--- player_manager.py
+++ player_manager.py
@@ -40,9 +40,9 @@ class ChallengeClient(showdown.Client):
         #uncomment below line to message user XX on init
         #await self.private_message('XX', 'hello there')
 
-        #uncomment below lines to run for 2 hours and then quit, also comment out room deinit
-        #await asyncio.sleep(7200)
-        #quit()
+        #uncomment below lines to not run for more than 2 hours after first connect
+        await asyncio.sleep(7200)
+        quit()
 
     async def on_private_message(self, pm):
         if pm.recipient == self:
@@ -88,15 +88,15 @@ class ChallengeClient(showdown.Client):
                 await self.warriors[room_id].room_obj.leave()
 
     async def on_room_deinit(self, room_obj):
-        user_inp = input('Do another battle / stay online (y/n)?').lower()
-        if user_inp == 'y':
-            await asyncio.sleep(0)
-
+        del self.warriors[room_obj.id]
+        #uncomment below lines to give the user a chance to exit on battle end
+        #user_inp = input('Do another battle / stay online (y/n)?').lower()
+        #if user_inp != 'y':
+        #    quit()
+        #else:
             #uncomment below to play another random battle on battle end
             #await self.search_battles('', 'gen1randombattle')
 
             #uncomment below line to message user XX on battle end
             #await self.private_message('XX', 'hello there')
-        else:
-            await asyncio.sleep(0)
-            quit()
+        await asyncio.sleep(0)
